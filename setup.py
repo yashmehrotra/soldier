7de4b72b0c5ef8b25a8db2c06c1bf802363b605f
Yash Mehrotra <yashmehrotra95@gmail.com>
2015-03-16 00:43:18 +0530

    setup added

---
+++ setup.py
@@ -0,0 +1,16 @@
+
+from distutils.core import setup
+
+setup(
+    name='soldier',
+    version='0.0.1',
+    author='Yash Mehrotra',
+    author_email='yashmehrotra95@gmail.com',
+    packages=['soldier'],
+    scripts=[],
+    url='https://pypi.python.org/pypi/soldier',
+    license='LICENSE',
+    description='Useful towel-related stuff.',
+    long_description=open('README.md').read(),
+    install_requires=[],
+)
