13e1becb0fd20ec6caa8274e503bf18f59764184
Yash Mehrotra <yashmehrotra95@gmail.com>
2015-03-16 01:17:29 +0530

    minor change for pip

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from distutils.core import setup
 
 setup(
     name='soldier',
-    version='0.0.1',
+    version='0.0.1.2',
     author='Yash Mehrotra',
     author_email='yashmehrotra95@gmail.com',
     packages=['soldier'],
     scripts=[],
     url='https://pypi.python.org/pypi/soldier',
     license='LICENSE',
-    description='Useful towel-related stuff.',
-    long_description=open('README.md').read(),
+    description='Subprocess with all guns blazing',
+    long_description='To be Added',
     install_requires=[],
 )
---
+++ soldier/__init__.py
@@ -0,0 +1,1 @@
+from soldier import Soldier
