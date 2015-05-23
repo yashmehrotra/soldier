0ebf29465d95446fe656c1498bf57762c0397669
Yash Mehrotra <yashmehrotra95@gmail.com>
2015-05-23 17:47:14 +0530

    Design Change

--- soldier/new.py
+++ soldier/new.py
@@ -6,6 +6,7 @@ from subprocess import (
         PIPE,
         Popen,
         STDOUT,
+        call,
         check_output,
     )
 import sys
@@ -16,16 +17,18 @@ class Soldier(object):
     Add docstring here bwahaaha
     """
     
-    def __init__(self,command):
+    def __init__(self,command,**kwargs):
         """
         The Constructor
         """
         
+        #self._command = Soldier._parse_command(command)
         self._command = command
         self._pid = None
         self._status_code = None
         self._pstart_ts = None
         self._pend_ts = None
+        self._output = kwargs.get('output')
 
     
     @staticmethod
@@ -38,8 +41,8 @@ class Soldier(object):
         return command
 
 
-    @classmethod
-    def output(Soldier,command,**kwargs):
+    @staticmethod
+    def output(command, **kwargs):
         """
         Possible kwargs:
         - split
@@ -56,9 +59,24 @@ class Soldier(object):
                         #shell=True Commented out as it has to be researched
                     )
             #self._command = command
-            return Soldier(command)
+            #return Soldier(command,output=output)
+            return output
         except CalledProcessError:
             return "Error"
+
+
+    @classmethod
+    def call(Soldier, command):
+        """
+        Call a process
+        """
+
+        shlex_command = Soldier._parse_command(command)
+
+        called_process = call(shlex_command)
+
+        return Soldier(command)
+    
 
     @property
     def command(self):
@@ -66,3 +84,21 @@ class Soldier(object):
         Return command
         """
         return self._command
+    
+
+    @property
+    def out(self):
+        """
+        Return output
+        """
+        return self._output
+
+def t_output(command):
+    """API
+    MAYBE"""
+
+    s = Soldier('fuck',output='suck')
+    s._status_code = 0
+
+    return s
+
